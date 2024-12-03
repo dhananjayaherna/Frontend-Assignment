c091dbef52f9ebe66f5be5b3b66538c26dde28d7
DHANANJAYA HERNA <dhirenjaeger@DHANANJAYAs-MacBook-Air.local>
2024-12-04 02:36:49 +0530

    changed APP.JS

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   const [data, setData] = useState(null);
   const [user, setUser] = useState(null);
   const [visible, setVisible] = useState(false);
-  const priority = ['No priority', 'Low', 'Medium', 'High', 'Urgent'];
-  const status = ['In progress', 'Todo', 'Backlog', 'Done', 'Cancelled'];
+
+  const status = useMemo(() => ['In progress', 'Todo', 'Backlog', 'Done', 'Cancelled'], []);
+  const priority = useMemo(() => ['No priority', 'Low', 'Medium', 'High', 'Urgent'], []);
   const [groupBy, setGroupBy] = useState('priority');
   const [orderBy, setOrderBy] = useState('title');
   const [groups, setGroups] = useState([]);
@@ -25,7 +26,7 @@ function App() {
         setUser(users);
         setData(tickets);
       });
-  }, [data, user, groupBy, orderBy]);
+  }, [data, user, status, groupBy, orderBy]);
 
   useMemo(() => {
     if (!data) return;
@@ -33,55 +34,53 @@ function App() {
       case 'priority': {
         setGroups(priority.map((pr, i) => ({
           title: pr,
-          group: data.filter(d => d.priority === i).sort((a, b) => (a[orderBy] < b[orderBy]) * -1
-          )
+          group: data.filter(d => d.priority === i).sort((a, b) => (a[orderBy] < b[orderBy]) ? -1 : 1)
         })).reverse());
         break;
       }
       case 'status': {
         setGroups(status.map(st => ({
           title: st,
-          group: data.filter(d => d.status === st).sort((a, b) => (a[orderBy] < b[orderBy]) * -1
-          )
+          group: data.filter(d => d.status === st).sort((a, b) => (a[orderBy] < b[orderBy]) ? -1 : 1)
         })));
         break;
       }
       case 'user': {
         setGroups(user.map(u => ({
           title: u.name,
-          group: data.filter(d => d.userId === u.id).sort((a, b) => (a[orderBy] < b[orderBy]) * -1
-          )
+          group: data.filter(d => d.userId === u.id).sort((a, b) => (a[orderBy] < b[orderBy]) ? -1 : 1)
         })));
         break;
       }
       default: {
         setGroups(status.map(st => ({
           title: st,
-          group: data.filter(d => d.status === st).sort((a, b) => (a[orderBy] < b[orderBy]) * -1
-          )
+          group: data.filter(d => d.status === st).sort((a, b) => (a[orderBy] < b[orderBy]) ? -1 : 1)
         })));
         break;
       }
     }
-  }, [data, groupBy, orderBy]);
-  return (
+  }, [data, groupBy, orderBy, priority, status, user]);
 
+  return (
     <div>
-      <button id='filter' onClick={() => setVisible(visible ? false : true)}> Display </button><br/>
+      <button id='filter' onClick={() => setVisible(!visible)}> Display </button><br />
       {visible && <div id='filter-box'>
         <b>Group By:</b> {['status', 'priority', 'user'].map(g => <>
-        <input name={'group-by'} value={g} type='radio' checked={g === groupBy} onClick={() => {setGroupBy(g);
-          localStorage.setItem('group-by',g)}
-        }/> <label for={g}>{g[0].toUpperCase() + g.slice(1)}</label>
-        &emsp;</>)}<br />
+          <input name={'group-by'} value={g} type='radio' checked={g === groupBy} onClick={() => {
+            setGroupBy(g);
+            localStorage.setItem('group-by', g);
+          }} /> <label htmlFor={g}>{g[0].toUpperCase() + g.slice(1)}</label>
+          &emsp;</>)}<br />
         <b>Order By:</b> {['title', 'priority'].map(o => <>
-        <input name={'order-by'} value={o} type='radio' checked={o === orderBy} onClick={() => {setOrderBy(o);
-          localStorage.setItem('order-by',o)}
-        }/> <label for={o}>{o[0].toUpperCase() + o.slice(1)}</label>
-        &emsp;</>)}
+          <input name={'order-by'} value={o} type='radio' checked={o === orderBy} onClick={() => {
+            setOrderBy(o);
+            localStorage.setItem('order-by', o);
+          }} /> <label htmlFor={o}>{o[0].toUpperCase() + o.slice(1)}</label>
+          &emsp;</>)}
       </div>}
       <div id='group-container'>
-        { groups.map(g => <Group title={g.title} group={g.group} />) }
+        {groups.map(g => <Group title={g.title} group={g.group} />)}
       </div>
     </div>
   );
